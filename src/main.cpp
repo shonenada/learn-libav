b52ac747ea58ebfcc59c1e79c51ecef3abc436bd
shonenada <shonenada@gmail.com>
2022-12-08 02:21:25 +0800

    feat: init basic cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <string>
-#include <libavcodec/avcodec.h>
+
+extern "C" {
 #include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+}
 
 int main() {
     std::string filename("demo.mp4");
     AVFormatContext *pFormatContext = avformat_alloc_context();
-    // avformat_open_input(&pFormatContext, filename.c_str(), NULL, NULL);
-    avformat_open_input(&pFormatContext, "./demo.mp4", NULL, NULL);
-
+    avformat_open_input(&pFormatContext, filename.c_str(), NULL, NULL);
 }
