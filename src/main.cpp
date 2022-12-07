acd82d9b597d38c3839b6f19e3da7fc1155a63f6
shonenada <shonenada@gmail.com>
2022-12-08 02:50:35 +0800

    feat: extract codec info from AVFormatContext

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,18 @@ int main() {
     std::string filename("demo.mp4");
     AVFormatContext *pFormatContext = avformat_alloc_context();
     avformat_open_input(&pFormatContext, filename.c_str(), NULL, NULL);
+    avformat_find_stream_info(pFormatContext, NULL);
+
+    for(int i=0; i<pFormatContext->nb_streams; i++) {
+        AVCodecParameters *pCP = pFormatContext->streams[i]->codecpar;
+        AVCodec *pCodec = avcodec_find_decoder(pCP->codec_id);
+
+        if (pCP->codec_type == AVMEDIA_TYPE_VIDEO) {
+            std::cout << "Video Codec: resolution " << pCP->width << "x" << pCP->height << " " << std::endl;
+        } else if (pCP->codec_type == AVMEDIA_TYPE_AUDIO) {
+            std::cout << "Audio Codec: " << pCP->channels << " channels, sample rate: " << pCP->sample_rate << std::endl;
+        }
+    }
+
     DumpAVFormat(pFormatContext);
 }
